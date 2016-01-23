f3118e1c509eac2ef6b4a16bda0415711950043b
Guillermo De los Santos <memo.dlsg@gmail.com>
2016-01-23 15:36:11 -0600

    Add empty husk of result endpoint

--- src/kool-server/kool-server.go
+++ src/kool-server/kool-server.go
@@ -16,12 +16,36 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	enc.Encode(&hello)
 }
 
+func result(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+	enc := json.NewEncoder(w)
+
+	dec := json.NewDecoder(r.Body)
+	var resultData map[string]interface{}
+	err := dec.Decode(&resultData)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		errorMessage := make(map[string]string)
+		errorMessage["message"] = "Invalid request"
+		enc.Encode(&errorMessage)
+		return
+	}
+
+	//TODO actually store the info
+	w.WriteHeader(http.StatusOK)
+	message := make(map[string]string)
+	message["message"] = "Correctly saved (not really)"
+	enc.Encode(&message)
+}
+
 func main() {
 	fmt.Println("Starting server!")
 
 	/* Initialize handlers */
 	router := mux.NewRouter()
 	router.HandleFunc("/hello", hello).Methods("GET")
+	router.HandleFunc("/result", result).Methods("POST")
 
 	n := negroni.Classic()
 	n.UseHandler(router)
