f8c2e3bb716498783251a8e5cb09467599abc5e4
Ivan Californias <ivan.californias@gmail.com>
2016-01-23 17:53:52 -0600

    Make travis deploy only for master branch.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,12 +3,21 @@
 RPM_FILE=$(basename `ls rpm/x86_64/kool-server-*`)
 SERVER="deploy@katdev.no-ip.biz"
 PORT="26"
-echo "Starting deploy..."
 
-echo "current directory in server"
+function dlog {
+	echo " => $1"
+}
+
+dlog "Starting deploy..."
+
+dlog "Enabling ssh-agent"
+eval "$(ssh-agent -s)"
+chmod 600 deploy/deploy_key
+ssh-add deploy/deploy_key
+dlog "current directory in server"
 ssh -p ${PORT} -o StrictHostKeyChecking=no ${SERVER} "pwd"
 ssh -p ${PORT} ${SERVER} "mkdir -p ~/working"
-echo "pushing package to prod server"
+dlog "pushing package to prod server"
 scp -P ${PORT} rpm/x86_64/${RPM_FILE} ${SERVER}:~/working
-echo "installing new version of kool-server"
+dlog "installing new version of kool-server"
 ssh -p ${PORT} -tt ${SERVER} "sudo yum install -y ~/working/${RPM_FILE} 2>&1"
