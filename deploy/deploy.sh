76328cdc135ab03764b2920acee47a5c922f6f28
Ivan Californias <ivan.californias@gmail.com>
2016-01-23 18:05:52 -0600

    Use 'before_deploy' section to enable ssh-agent and skip cleanup.
    By default deploy phase cleans up the workingspace before deploying.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -10,14 +10,12 @@ function dlog {
 
 dlog "Starting deploy..."
 
-dlog "Enabling ssh-agent"
-eval "$(ssh-agent -s)"
-chmod 600 deploy/deploy_key
-ssh-add deploy/deploy_key
-dlog "current directory in server"
+dlog "Current directory in server"
 ssh -p ${PORT} -o StrictHostKeyChecking=no ${SERVER} "pwd"
 ssh -p ${PORT} ${SERVER} "mkdir -p ~/working"
-dlog "pushing package to prod server"
+
+dlog "Pushing package to prod server"
 scp -P ${PORT} rpm/x86_64/${RPM_FILE} ${SERVER}:~/working
-dlog "installing new version of kool-server"
+
+dlog "Installing new version of kool-server"
 ssh -p ${PORT} -tt ${SERVER} "sudo yum install -y ~/working/${RPM_FILE} 2>&1"
